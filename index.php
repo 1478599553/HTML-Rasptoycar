62ef5f58d94e3804cd055c203f6284fe952eaeab
1478599553 <64693215+1478599553@users.noreply.github.com>
2020-10-17 14:25:20 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -14,7 +14,23 @@ body
 <script src="jquery-3.5.1.js"></script>
 <body> 
 
-
+<script type="text/javascript" language=JavaScript charset="UTF-8">
+      document.onkeydown=function(event){
+            var e = event || window.event || arguments.callee.caller.arguments[0];
+            if(e && e.keyCode==87){ // 按 w
+                window.location.href="writer^.php";
+              }
+            if(e && e.keyCode==65){ // 按 a
+                 window.location.href="writer<.php";
+               }
+            if(e && e.keyCode==83){ // 按 s
+                 window.location.href="writerv.php";
+            }
+	    if(e && e.keyCode==68){ // 按 d
+                 window.location.href="writer>.php";
+            }
+        };
+</script>
 
 <style>
 .center
@@ -44,19 +60,19 @@ body
 </div>
 
 <div class="center">
-<a href="writer^.php"><button>^</button></a>
+<a href="writer^.php"><button>^</button></a> <!写入1>
 </div>
 
 <div class="left">
-<a href="writer<.php"><button><</button></a>
+<a href="writer<.php"><button><</button></a> <!写入2>
 </div>
 
 <div class="right">
-<a href="writer>.php"><button>></button></a>
+<a href="writer>.php"><button>></button></a> <!写入3>
 </div>
 
 <div class="center">
-<a href="writerv.php"><button>v</button></a>
+<a href="writerv.php"><button>v</button></a> <!写入4>
 </div>
 
 </body> 
